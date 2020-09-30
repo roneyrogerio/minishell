4d1ccac8efe064b3c64f739184f8f6b6c02d49dc
Roney <roneyrogerio92@gmail.com>
2020-09-30 12:16:09 -0300

    Adds libft header file.

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: rde-oliv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/27 09:09:46 by rde-oliv          #+#    #+#             */
-/*   Updated: 2020/09/27 09:22:06 by rde-oliv         ###   ########.fr       */
+/*   Updated: 2020/09/30 12:13:28 by rde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 # define MINISHELL_H
 # include <unistd.h>
 # include "get_next_line.h"
+# include "libft/libft.h"
 
 #endif
