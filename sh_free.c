851575ba0f7f002a2b5fa945f9faae4c22d2e146
Roney <roneyrogerio92@gmail.com>
2020-10-22 18:24:57 -0300

    Free the multiline cache and reset variables.

--- sh_free.c
+++ sh_free.c
@@ -6,7 +6,7 @@
 /*   By: rde-oliv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/11 13:53:41 by rde-oliv          #+#    #+#             */
-/*   Updated: 2020/10/22 09:24:23 by rde-oliv         ###   ########.fr       */
+/*   Updated: 2020/10/22 17:50:24 by rde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ void	sh_free(void)
 {
 	tokens_clear();
 	free(g_lx.line);
+	free(g_lx.cache);
 	g_lx.line = NULL;
 	g_tokens = NULL;
+	g_lx.cache = NULL;
+	g_lx.qte = 0;
+	g_lx.dqte = 0;
 }
