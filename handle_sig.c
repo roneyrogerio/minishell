727f758324509ebbfcd0a0b618abf14487ecf4aa
Roney <roneyrogerio92@gmail.com>
2020-11-22 11:37:48 -0300

    Only do SIGQUIT if you have a program running.

--- handle_sig.c
+++ handle_sig.c
@@ -6,7 +6,7 @@
 /*   By: rde-oliv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/13 18:23:19 by rde-oliv          #+#    #+#             */
-/*   Updated: 2020/11/22 11:26:02 by rde-oliv         ###   ########.fr       */
+/*   Updated: 2020/11/22 11:34:57 by rde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,9 @@ void	handle_sig(int sig)
 	{
 		wait(NULL);
 	}
-	else if (sig == SIGQUIT && (g_sh.status = 131))
+	else if (g_sh.ast && sig == SIGQUIT && (g_sh.status = 131))
 	{
-		if (g_sh.ast)
-			ft_putstr_fd("Quit", 1);
-		ft_putchar_fd('\n', 1);
-		if (!g_sh.ast)
-			ft_putstr_fd("$ ", 1);
+		ft_putstr_fd("Quit\n", 1);
 		kill_pids(SIGQUIT);
 		sh_free();
 	}
