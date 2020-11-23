5c3b659c05f869acfc8b5ad92368b6bc9c08b2c9
Roney <roneyrogerio92@gmail.com>
2020-11-23 11:13:39 -0300

    Imprement builtin unset command.

--- builtins.h
+++ builtins.h
@@ -6,7 +6,7 @@
 /*   By: rde-oliv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 20:31:02 by rde-oliv          #+#    #+#             */
-/*   Updated: 2020/11/23 00:23:37 by rde-oliv         ###   ########.fr       */
+/*   Updated: 2020/11/23 11:02:07 by rde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@ int		int_env(void);
 int		int_cd(int i);
 int		int_export(int i);
 void	set_env_exp(char *name);
+int		int_unset(int i);
+void	unset_env(char *name);
 
 #endif
--- builtins_utils.c
+++ builtins_utils.c
@@ -6,7 +6,7 @@
 /*   By: rde-oliv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 00:02:04 by rde-oliv          #+#    #+#             */
-/*   Updated: 2020/11/23 01:41:28 by rde-oliv         ###   ########.fr       */
+/*   Updated: 2020/11/23 11:03:48 by rde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,47 @@ void	set_env_exp(char *name)
 			env->content->exp = 1;
 			break ;
 		}
+		env = env->next;
+	}
+}
+
+int		int_unset(int i)
+{
+	int	j;
+
+	j = 1;
+	while (g_sh.ast[i].argv[j])
+	{
+		unset_env(g_sh.ast[i].argv[j]);
+		j++;
+	}
+	return (1);
+}
+
+void	unset_env(char *name)
+{
+	t_env_lst	*env;
+	t_env_lst	*prev;
+	char		*var;
+
+	env = g_sh.env;
+	var = NULL;
+	prev = NULL;
+	while (env)
+	{
+		var = env_start(env->content->value, name, ft_strlen(name));
+		if (var != NULL)
+		{
+			if (prev)
+				prev->next = env->next;
+			else
+				g_sh.env = env->next;
+			free(env->content->value);
+			free(env->content);
+			free(env);
+			break ;
+		}
+		prev = env;
 		env = env->next;
 	}
 }
