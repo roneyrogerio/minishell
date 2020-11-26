2753823a706186195aa62a212ded7110adf0baeb
Roney <roneyrogerio92@gmail.com>
2020-11-26 12:04:31 -0300

    It makes possible to reuse the function.

--- env_join.c
+++ env_join.c
@@ -6,7 +6,7 @@
 /*   By: rde-oliv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 14:48:27 by rde-oliv          #+#    #+#             */
-/*   Updated: 2020/11/21 23:17:38 by rde-oliv         ###   ########.fr       */
+/*   Updated: 2020/11/26 11:57:47 by rde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int		env_join(int i)
 	{
 		name_len = env_join_name_len(g_sh.ast[i].env[j]);
 		find = env_var(g_sh.ast[i].env[j], name_len);
-		if (find == NULL && !env_join_new(i, j))
+		if (find == NULL && !env_join_new(g_sh.ast[i].env[j], 0))
 			return (0);
 		else if (!env_join_same(i, j))
 			return (0);
@@ -32,7 +32,7 @@ int		env_join(int i)
 	return (1);
 }
 
-int		env_join_new(int i, int j)
+int		env_join_new(char *env, int exp)
 {
 	t_env_lst	*item;
 	t_env		*content;
@@ -40,14 +40,14 @@ int		env_join_new(int i, int j)
 	item = NULL;
 	if ((content = malloc(sizeof(t_env))) == NULL ||
 		(item = ft_lstnew(content)) == NULL ||
-		(content->value = ft_strdup(g_sh.ast[i].env[j])) == NULL)
+		(content->value = ft_strdup(env)) == NULL)
 	{
 		g_errno = SH_MEMERR;
 		free(content);
 		free(item);
 		return (0);
 	}
-	content->exp = 0;
+	content->exp = exp;
 	ft_lstadd_back((void **)&g_sh.env, item);
 	return (1);
 }
--- env_join.h
+++ env_join.h
@@ -6,7 +6,7 @@
 /*   By: rde-oliv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 15:43:00 by rde-oliv          #+#    #+#             */
-/*   Updated: 2020/11/21 22:41:39 by rde-oliv         ###   ########.fr       */
+/*   Updated: 2020/11/26 11:59:34 by rde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define ENV_JOIN_H
 
 int		env_join(int i);
-int		env_join_new(int i, int j);
+int		env_join_new(char *env, int exp);
 int		env_join_same(int i, int j);
 int		env_join_name_len(char *str);
 
